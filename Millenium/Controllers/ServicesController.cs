4687cef4e567b0e1bb174d0e78dd45803ced8b60
alex_e <alex_e@holdingbp.ru>
2020-04-03 19:37:22 +0500

    осталось кровельные работы

---
+++ Millenium/Controllers/ServicesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Millenium.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult RemontPodezda()
+        {
+            return View();
+        }
+        public IActionResult Asfaltirovanie()
+        {
+            return View();
+        }
+        public IActionResult Blagoustroistvo()
+        {
+            return View();
+        }
+        public IActionResult KrovelnieRaboti()
+        {
+            return View();
+        }
+        public IActionResult ZamenaOkonIDverey()
+        {
+            return View();
+        }
+        public IActionResult RemontFasadov()
+        {
+            return View();
+        }
+    }
+}
