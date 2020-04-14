0301d397572c8b0aabe35f3c63c7898b604e2ad2
alex_e <alex_e@holdingbp.ru>
2020-04-14 21:14:27 +0500

    форма отправки + мелоч

--- Millenium/Controllers/HomeController.cs
+++ Millenium/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Millenium.Controllers
 {
@@ -31,6 +33,26 @@ namespace Millenium.Controllers
         public IActionResult Price()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(string phoneNumber, string name)
+        {
+            MailMessage message = new MailMessage();
+            SmtpClient smtp = new SmtpClient();
+            message.From = new MailAddress("lexatorgas@gmail.com");
+            message.To.Add(new MailAddress("rsk.millenium@gmail.com"));
+            message.Subject = "Test";
+            message.IsBodyHtml = true; //to make message body as html  
+            message.Body = $"Пришла заявка с сайта номер = {phoneNumber}, имя = {name}";
+            smtp.Port = 587;
+            smtp.Host = "smtp.gmail.com";
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("lexatorgas@gmail.com", "1234usehon");
+            smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+            smtp.Send(message);
+            return View("Index");
         }
     }
 }
